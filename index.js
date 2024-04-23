f5b716d5cb58dc48fdb4659ffe7401a521e501ad
Daniel Bradley <daniel@pulumi.com>
2024-04-23 11:37:48 +0100

    Enable setting version result as an environment variable

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import { calculateVersion } from "./version";
 try {
   const version = await calculateVersion(github.context);
   core.setOutput("version", version);
+  const envVar = core.getInput("set-env");
+  if (envVar !== "") {
+    core.exportVariable(envVar, version);
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
